0cf6f648344bf1a2272daccb460bd378d136de9e
Bluperman949 <christiandoolittle@hotmail.com>
2026-08-22 14:05:34 -0700

    rework gradle Scanner to include subprojects

--- lua/maker/default_scanners/gradle.lua
+++ lua/maker/default_scanners/gradle.lua
@@ -2,11 +2,24 @@ local scanners = require'maker.scanners'
 local util = require'maker.util'
 
 return scanners.create('gradle', function ()
-  local file = util.find_file'gradlew'
-  if not file then return nil end
-  local response = util.shell_command'./gradlew tasks'
-  return util.tbl_map_drop_nil(function (line)
-    local match = line:match('^%l%a+ -')
-    return match and './gradlew ' .. match:sub(0, -1)
-  end, response)
+  local files = vim.fs.find({ 'build.gradle', 'build.gradle.kts' }, { limit = 64 })
+  if #files == 0 then return nil end
+  local exe = vim.fs.find('gradlew')[1] and './gradlew ' or 'gradle '
+
+  local tasks = {}
+
+  for _,file in pairs(files) do
+    local project = vim.fn.fnamemodify(file, ':.:h')
+    project = project == '.' and '' or ':' .. project
+
+    local response = util.shell_command(exe .. project .. ':tasks')
+    local project_tasks = util.tbl_map_drop_nil(function (line)
+      local match = line:match('^%l%a+ -')
+      return match and exe .. project .. ':' .. match:sub(0, -1)
+    end, response)
+
+    vim.list_extend(tasks, project_tasks)
+  end
+
+  return tasks
 end)
